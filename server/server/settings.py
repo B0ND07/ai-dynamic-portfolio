762aeb8ac72808855acfb69cf095cb037371c114
b0nd07 <mhashimkp@gmail.com>
2026-07-26 21:38:15 +0530

    feat: configure CSRF trusted origins dynamically

--- server/server/settings.py
+++ server/server/settings.py
@@ -141,7 +141,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
@@ -154,6 +153,11 @@ CORS_ALLOWED_ORIGINS = os.getenv(
 ).split(',')
 
 CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = os.getenv(
+    'CSRF_TRUSTED_ORIGINS',
+    'http://localhost:5173,http://localhost:8080,http://127.0.0.1:5173,http://127.0.0.1:8080'
+).split(',')
 
 # Security settings for production
 if not DEBUG:
